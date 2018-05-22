52e34137cfd355f2ff2ac231b2d5ffa74286894d
Mario Nitchev <marionitchev@gmail.com>
2018-05-22 13:33:05 +0200

    add BOSH_DIRECT env var
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- tasks/delete-cf-deployment.sh
+++ tasks/delete-cf-deployment.sh
@@ -10,4 +10,4 @@ export BOSH_CLIENT_SECRET=`bosh int $DIRECTOR_PATH/vars.yml --path /admin_passwo
 ./ci-resources/scripts/setup-env.sh
 ./ci-resources/scripts/bosh-login.sh
 
-bosh -e lite -d cf delete-deployment
+bosh -e lite -d cf -n delete-deployment
