3124be888adc32534e1361d8d8f27cfbe0e706a6
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-05-22 15:45:48 +0200

    fix path to manifest yaml

--- tasks/upload-stemcell.sh
+++ tasks/upload-stemcell.sh
@@ -10,7 +10,7 @@ export BOSH_CLIENT_SECRET=`bosh int $DIRECTOR_PATH/vars.yml --path /admin_passwo
 ./ci-resources/scripts/setup-env.sh
 ./ci-resources/scripts/bosh-login.sh
 
-STEMCELL_VERSION=$(bosh int manifest.yml --path /releases/name=capi/stemcell/version)
+STEMCELL_VERSION=$(bosh int out/manifest.yml --path /releases/name=capi/stemcell/version)
 
 echo "::::::::::::::UPLOAD-STEMCELL-VERSION: $STEMCELL_VERSION"
 bosh -e lite upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent\?v\=$STEMCELL_VERSION
